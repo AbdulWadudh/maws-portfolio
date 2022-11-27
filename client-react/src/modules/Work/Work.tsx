134aedb20a90fd2f6b0b35b1e05f376939f9486f
Mohammed Abdul Wadudh Samdani <abdulwadudh5@gmail.com>
2022-11-27 13:27:21 +0530

    feat: :sparkles: Added Work Section

--- client-react/src/modules/Work/Work.tsx
+++ client-react/src/modules/Work/Work.tsx
@@ -1,9 +1,104 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
 
+import { client, urlFor } from "../../client";
+import { AppWrap } from "../../wrapper";
+
+import { AnimateType, WorkSection } from "./WorkSecTypes";
 import "./work.scss";
 
 const Work = () => {
-    return <div>Work</div>;
+    const [activeFilter, setActiveFilter] = useState("All");
+    const [animateCard, setAnimateCard] = useState<AnimateType>({ y: 0, opacity: 1 });
+    const [works, setWorks] = useState([]);
+    const [filterWork, setFilterWork] = useState([]);
+
+    const handleFilter = (item: string) => {
+        
+        setActiveFilter(item);
+        setAnimateCard({ y: 100, opacity: 0 });
+
+        setTimeout(() => {
+            setAnimateCard({ y: 0, opacity: 1 });
+            if (item === "All") setFilterWork(works);
+            else setFilterWork(works?.filter((work: WorkSection) => work.tags.includes(item)));
+        }, 500);
+    };
+
+    useEffect(() => {
+        (async () => {
+            const worksData = await client.fetch(`*[_type == "works"]`);
+            setWorks(worksData);
+            setFilterWork(worksData);
+        })();
+    }, []);
+
+    return (
+        <>
+            <h2 className="head-text">
+                My Collective <span>Portfolio</span> Section.
+            </h2>
+            <div className="app__work-filter">
+                {["UI/UX", "Web App", "Mobile App", "React JS", "All"].map((item, idx) => (
+                    <div
+                        key={idx}
+                        onClick={() => handleFilter(item)}
+                        className={`app__work-filter-item app__flex p-text ${activeFilter === item ? "item-active" : ""}`}
+                    >
+                        {item}
+                    </div>
+                ))}
+            </div>
+
+            <motion.div animate={animateCard} transition={{ duration: 0.5, delayChildren: 0.5 }} className="app__work-portfolio">
+                {filterWork?.map(({ imgUrl, description, tags, title, projectLink, codeLink }: WorkSection, idx) => (
+                    <div className="app__work-item app__flex" key={idx}>
+                        <div className="app__work-img app__flex">
+                            {" "}
+                            <img src={urlFor(imgUrl).url()} alt={title} />
+                            <motion.div
+                                whileHover={{ opacity: [0, 1] }}
+                                transition={{ duration: 0.25, ease: "easeInOut", staggerChildren: 0.5 }}
+                                className="app__work-hover app__flex"
+                            >
+                                <a href={projectLink} target="_blank" rel="noreferr">
+                                    <motion.div
+                                        whileHover={{ scale: [1, 0.9] }}
+                                        whileInView={{ scale: [0, 1] }}
+                                        transition={{ duration: 0.25 }}
+                                        className="app__flex"
+                                    >
+                                        <AiFillEye />
+                                    </motion.div>
+                                </a>
+                                <a href={codeLink} target="_blank" rel="noreferr">
+                                    <motion.div
+                                        whileHover={{ scale: [1, 0.9] }}
+                                        whileInView={{ scale: [0, 1] }}
+                                        transition={{ duration: 0.25 }}
+                                        className="app__flex"
+                                    >
+                                        <AiFillGithub />
+                                    </motion.div>
+                                </a>
+                            </motion.div>
+                        </div>
+                        <div className="app__work-content app__flex">
+                            <h4 className="bold-text">{title}</h4>
+                            <p className="p-text" style={{ marginTop: 10 }}>
+                                {description}
+                            </p>
+
+                            <div className="app__work-tag app__flex">
+                                <p className="p-text">{tags[0]}</p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </motion.div>
+        </>
+    );
 };
 
-export default Work;
+export default AppWrap(Work, "work");
